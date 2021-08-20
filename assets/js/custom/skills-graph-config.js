64fb1809d8a0f6fdefca37d7dfd22de51ad6040c
bony237 <nbj.ngoube@gmail.com>
2021-08-20 08:26:31 +0100

    task 2 frontend completed

--- assets/js/custom/skills-graph-config.js
+++ assets/js/custom/skills-graph-config.js
@@ -1,22 +1,28 @@
 const DATA_COUNT = 7;
-const NUMBER_CFG = { count: DATA_COUNT, min: 0, max: 10 };
+const NUMBER_CFG = { count: DATA_COUNT, min: 0, max: 5 };
 
 const labels = [
   "Frontend",
-  "Backend",
   "Mobile",
-  "DB & ORM",
   "Cloud computing",
   "Agile project management",
+  "DB & ORM",
+  "Backend",
 ];
 const data = {
   labels: labels,
   datasets: [
     {
       label: "My Stack",
-      data: [9, 7, 5, 7, 3, 5],
+      data: [5, 4, 3, 4, 2.5, 2.5],
       borderColor: "#538135",
       backgroundColor: "rgba(130,214,40,0.2)",
+    },
+    {
+      label: "skip",
+      data: [0, 5, 5, 0, 3, 5],
+      borderColor: "rgba(130,214,40,0)",
+      backgroundColor: "rgba(130,214,40,0)",
     },
   ],
 };
@@ -24,6 +30,10 @@ const data = {
 const config = {
   type: "radar",
   data: data,
+  scaleOverride: true,
+  scaleSteps: 1,
+  
+  scaleStartValue: 0,
   options: {
     responsive: true,
     plugins: {
@@ -32,6 +42,7 @@ const config = {
         text: "Skills Radar",
       },
     },
+    
   },
 };
 export { config };
--- print_name.js
+++
@@ -1,1 +0,0 @@
-console.log('NGOUNOU NGOUBEYOU Bony Junior')
